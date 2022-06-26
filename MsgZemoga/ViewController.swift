d63dd80d2a1ae3d3ce913383b158f153f98aa59d
Christopher Gallego <ch.gallego06@gmail.com>
2022-06-26 11:55:10 -0400

    Init Setup

---
+++ MsgZemoga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MsgZemoga
+//
+//  Created by user222400 on 6/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
